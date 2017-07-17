a318c42318ae824368428415c68128037c1197dc
admin <admin@bitaps.com>
2017-07-17 23:04:04 +0400

    setup file

---
+++ connector/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# coding: utf-8
+from distutils.core import setup
+
+setup(name='Bitcoind  node connector',
+      version='0.0.1',
+      description='Connector to bitcoind node.',
+      author='Aleksey Karpov',
+      author_email='admin@bitaps.com',
+      url='https://github.com/bitaps-com/bitcoin-core-connector.git',
+      packages=['bitcoindconnector', ],
+     )
